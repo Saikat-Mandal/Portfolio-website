dde3ae519a9973f99f0f6a79fc43ecc89ce2a268
Saikat Mandal <msaikat022@gmail.com>
2025-04-19 10:39:57 +0530

    removed import dotring css

--- src/app/components/Dotring/Dotring.tsx
+++ src/app/components/Dotring/Dotring.tsx
@@ -1,27 +1,45 @@
-
 "use client";
 import useMousePosition from "@/app/hooks/useMousePosition";
-import "./dotring.css"
 
 const DotRing = () => {
-    // 1.
     const { x, y } = useMousePosition();
+
+    const ringStyle = {
+        position: "fixed",
+        width: "30px",
+        height: "30px",
+        border: "2px solid rgba(31, 30, 30, 0.808)",
+        borderRadius: "100%",
+        transform: `translate(-50%, -50%)`,
+        transitionDuration: "30ms",
+        transitionTimingFunction: "linear",
+        willChange: "width, height, transform, border",
+        zIndex: 999,
+        pointerEvents: "none",
+        transformOrigin: "center",
+        top: `${y}px`,
+        left: `${x}px`
+    };
+
+    const dotStyle = {
+        position: "fixed",
+        top: `${y}px`,
+        left: `${x}px`,
+        width: "16px",
+        height: "16px",
+        backgroundColor: "black",
+        borderRadius: "100%",
+        transform: "translate(-50%, -50%)",
+        zIndex: 999,
+        pointerEvents: "none"
+    };
+
     return (
         <>
-            {/* 2. */}
-            <div
-                style={{ left: `${x}px`, top: `${y}px` }}
-                className="ring"
-            ></div>
-            {/* 3. */}
-            <div
-                className="dot"
-                style={{ left: `${x}px`, top: `${y}px` }}
-            ></div>
+            <div style={ringStyle}></div>
+            <div style={dotStyle}></div>
         </>
     );
 };
 
 export default DotRing;
-
-
